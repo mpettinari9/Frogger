e87d8029d6fb0c1ecbdf7e690de8978c4f14b359
Federica <f.dimichele1@campus.uniurb.it>
2026-04-10 10:45:35 +0200

    Add name input and tutorial button to TitlePanel.

--- src/view/TitlePanel.java
+++ src/view/TitlePanel.java
@@ -5,6 +5,9 @@ import javax.swing.*;
 
 public class TitlePanel extends JPanel {
 	private JButton startButton;
+	private JButton tutorialButton;
+	private JTextField nameField;
+	private JLabel nameLabel;
 	
 	public TitlePanel(int screenWidth, int screenHeight) {
 		this.setSize(screenWidth, screenHeight);
@@ -15,10 +18,46 @@ public class TitlePanel extends JPanel {
 		this.startButton.setFont(new Font("DIALOG", Font.BOLD, 24));
 		this.startButton.setBounds(screenWidth/3, screenHeight*6/10, screenWidth/3, screenHeight/10);
 		this.add(this.startButton);
+		
+        this.nameLabel = new JLabel("Inserisci il tuo nome:");
+        this.nameLabel.setFont(new Font("DIALOG", Font.BOLD, 24));
+        this.nameLabel.setHorizontalAlignment(JLabel.CENTER);
+        this.nameLabel.setBounds(screenWidth/3, screenHeight*4/10, screenWidth/3, screenHeight/10);
+      	
+		this.nameField = new JTextField();
+		this.nameField.setBackground(Color.green);
+		this.nameField.setHorizontalAlignment(JTextField.CENTER);
+		this.nameField.setFont(new Font("DIALOG", Font.BOLD, 24));
+		this.nameField.setBounds(screenHeight/3, screenHeight*5/10, screenWidth/3, screenHeight/10);
+		
+		this.tutorialButton = new JButton("TUTORIAL");
+		this.tutorialButton.setBackground(Color.BLUE);
+		this.tutorialButton.setFont(new Font("DIALOG", Font.BOLD, 24));
+		this.tutorialButton.setBounds(screenHeight/3, screenHeight*8/10, screenWidth/3, screenHeight/10);
+		
+		this.add(nameLabel);
+		this.add(nameField);
+		this.add(tutorialButton);
 	}
 	
 	public JButton getStartButton(){
 		return startButton;
+	}
+	
+	public JButton getTutorialButton() {
+		return tutorialButton;
+	}
+
+	public JTextField getNameField() {
+		return nameField;
+	}
+	
+	public String getFrogName() {
+        return nameField.getText().trim();
+    }
+	
+	public JLabel getNameLabel() {
+		return nameLabel;
 	}
 	
 	protected void paintComponent(Graphics g) {
