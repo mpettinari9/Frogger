c5b090c70735176d62465ecba50d3c158608f42f
Federica <f.dimichele1@campus.uniurb.it>
2026-04-20 10:24:30 +0200

    Improved ResultPanel interface with background, title and layout.

--- src/view/ResultPanel.java
+++ src/view/ResultPanel.java
@@ -4,34 +4,54 @@ import java.awt.*;
 import javax.swing.*;
 
 public class ResultPanel extends JPanel {
-	private JLabel frogResultLabel;
-	private JButton toTitlePanelButton; // da implementare!!!!
+	private JLabel title;
+	private JLabel frogResultText;
+	private JButton toTitlePanelButton; 
 	
 	public ResultPanel(int screenWidth, int screenHeight) {
 		this.setSize(screenWidth, screenHeight);
 		this.setLayout(null);
-		this.setBackground(Color.black);
 	
-		this.frogResultLabel = new JLabel("");
-		this.frogResultLabel.setForeground(Color.white);
-		this.frogResultLabel.setFont(new Font("DIALOG",Font.BOLD, 25));
-		this.frogResultLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		this.frogResultLabel.setBounds(0, screenHeight/3, screenWidth, 50);
-		this.add(frogResultLabel);
+		this.title = new JLabel("");
+		this.frogResultText = new JLabel("");
 		
-		this.toTitlePanelButton = new JButton("TORNA AL MENU'");
-		this.toTitlePanelButton.setFont(new Font("DIALOG", Font.BOLD, 24));
-		this.toTitlePanelButton.setBounds(screenWidth / 3, screenHeight * 8/10, screenWidth /3, screenHeight / 10);
-		this.add(toTitlePanelButton);
+		this.title.setBounds(0, screenHeight/5, screenWidth, screenHeight/5);
+		this.title.setFont(new Font("DIALOG", Font.BOLD, 60));
+		this.title.setForeground(Color.WHITE);
+		this.title.setHorizontalAlignment(JLabel.CENTER);
 		
+		this.frogResultText.setBackground(Color.gray);
+		this.frogResultText.setHorizontalAlignment(JLabel.CENTER);
+		this.frogResultText.setVerticalAlignment(JLabel.CENTER);
+		this.frogResultText.setBounds(0, screenHeight*2/5, screenWidth, screenHeight/5);
+		
+		this.toTitlePanelButton = new JButton("RICOMINCIA");
+		this.toTitlePanelButton.setBackground(Color.blue);
+		this.toTitlePanelButton.setFont(new Font("DIALOG", Font.BOLD,24));
+		this.toTitlePanelButton.setBounds(screenWidth/3, screenHeight*6/10, screenWidth/3, screenHeight/10);
+		
+		this.add(this.title);
+		this.add(this.frogResultText);
+		this.add(this.toTitlePanelButton);
 	}
-	public JButton getToTitlePanelButton() {
-		return toTitlePanelButton;
+	
+	public void setTitle(String titleText) {
+	    this.title.setText(titleText);
 	}
 
 	public void setFrogResultText(String text) {
-		this.frogResultLabel.setText(text);	}
+		this.frogResultText.setText(text);	}
 	
+	private void setResultText(JLabel resultText, String result) {
+		resultText.setText(result);
+	}
 	
-
+	public JButton getToTitlePanelButton() {
+		return toTitlePanelButton;
+	}
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(new ImageIcon("src/view/Asset/sfondofrogger.png").getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+	}
 }
