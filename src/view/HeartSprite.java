05317049f6c931339cb5a6500b73f0987075eef2
Federica <f.dimichele1@campus.uniurb.it>
2026-04-20 14:45:00 +0200

    Refactor HeartSprite: extend JLabel, add life collection state.

--- src/view/HeartSprite.java
+++ src/view/HeartSprite.java
@@ -1,9 +1,28 @@
 package view;
 
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
-public class HeartSprite extends Sprite{
-	public HeartSprite(String imagePath) {
-		super(imagePath,1);
-	}
+public class HeartSprite extends JLabel{
+	
+	private boolean gainedLife;
+    
+    public HeartSprite(String heartPath) {
+        ImageIcon heartIcon = new ImageIcon(heartPath);
+        this.setIcon(heartIcon);
+        this.setOpaque(false);
+        this.setSize(heartIcon.getIconWidth(), heartIcon.getIconHeight());
+        
+        this.gainedLife = false;
+        this.setVisible(true);
+    }
+
+    public void gainLife() {
+        this.gainedLife = true;
+        this.setVisible(false);
+    }
+    
+    public boolean hasGainedLife() {
+        return gainedLife;
+    }
 }
