d40980a207c5b3611021459918ff169da82d29d6
MatiPettinari <matildepettinari2004@gmail.com>
2026-04-22 10:07:47 +0200

    Add tests for Frog movement

--- src/jUnitTest/JUnitTests.java
+++ src/jUnitTest/JUnitTests.java
@@ -14,4 +14,34 @@ public class JUnitTests {
 		map = new Map(1280, 720, 40, 290);
 		frog = new Frog("Test", Direction.UP, new Size(50, 50), new Position(615, 565), map, 2);
 	}
+	
+	@Test
+	public void testMoveUp() {
+		int initialY = frog.getY();
+		frog.moveUp();
+		assertTrue(frog.getY() < initialY);
+		assertEquals(Direction.UP, frog.getDirection());
+	}
+	
+	public void testMoveDown() {
+		int initialY = frog.getY();
+		frog.moveDown();
+		assertTrue(frog.getY() > initialY);
+		assertEquals(Direction.DOWN, frog.getDirection());
+	}
+	
+	public void testMoveLeft() {
+		int initialX = frog.getX();
+		frog.moveLeft();
+		assertTrue(frog.getX() < initialX);
+		assertEquals(Direction.LEFT, frog.getDirection());
+	}
+	
+	public void testMoveRight() {
+		int initialX = frog.getX();
+		frog.moveRight();
+		assertTrue(frog.getX() > initialX);
+		assertEquals(Direction.RIGHT, frog.getDirection());
+	}
+	
 }
