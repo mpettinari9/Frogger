58d8118ebe8a11eb84ca126ebdad2f50ea9f0f42
mpettinari9 <matildepettinari2004@gmail.com>
2026-08-07 09:37:23 +0200

    Refactor Heart

--- src/model/Heart.java
+++ src/model/Heart.java
@@ -2,7 +2,7 @@ package model;
 
 //Cuore che il giocatore può raccogliere per ottenere una vita extra.
 //Viene spawnato casualmente sulla mappa ogni 15 secondi.
-public class Heart {
+public class Heart implements Collectible {
 	//Dimensioni fisse del cuore (60x60 pixel)
 	public static final int HEART_WIDTH = 60;
     public static final int HEART_HEIGHT = 60;
@@ -11,11 +11,13 @@ public class Heart {
 
 	private final Position position; //Posizione sulla mappa
 	private HitBox hitBox; //Rilevamento collisione con rana
+	private boolean active;
 	
 	//Costruttore con oggetto posizione
 	public Heart(Position position) {
 		this.position = position;	
 		this.hitBox = new HitBox(position.getX(), position.getY(), HEART_WIDTH, HEART_HEIGHT); 
+		this.active = true;
 	}
 	
 	//Costruttore con coordinate
@@ -23,6 +25,22 @@ public class Heart {
 		this(new Position(x,y));
 	}
 	
+	@Override
+	public void onCollect(Frog frog, Game game) {
+	    frog.resetLives();
+	    this.active = false;
+    }
+
+	@Override
+	public boolean isActive() {
+	    return active;
+	}
+
+    @Override
+	public HitBox getHitBox() {
+	    return hitBox;
+	}
+	    
 	//Getter
 	public Position getPosition() {
 		return position;
@@ -36,9 +54,6 @@ public class Heart {
 		return position.getY();
 	}
 	
-	public HitBox getHitBox() {
-        return hitBox;
-    }
 	
 	public static int getHeartValue() {
 		return HEART_VALUE;
