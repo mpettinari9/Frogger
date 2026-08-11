c32f57ae0405428247ff17876ab03151bf696f67
mpettinari9 <matildepettinari2004@gmail.com>
2026-08-11 15:46:36 +0200

    add constructor in TitlePanel class

--- src/view/TitlePanel.java
+++ src/view/TitlePanel.java
@@ -34,5 +34,79 @@ public class TitlePanel extends JPanel {
 	private static final Color GIALLO_TESTO = new Color(255, 220, 0);
 	private static final Color BIANCO   = Color.WHITE; 
 	
+	public TitlePanel(int screenWidth, int screenHeight) {
+		this.setSize(screenWidth, screenHeight);
+		this.setLayout(null);
 
+		// --- Etichetta "Giocatori" ---
+		JLabel playersLabel = new JLabel("NUMERO DI GIOCATORI:");
+		playersLabel.setFont(new Font("DIALOG", Font.BOLD, 18));
+		playersLabel.setForeground(GIALLO_TESTO);
+		playersLabel.setHorizontalAlignment(JLabel.CENTER);
+		playersLabel.setBounds(screenWidth / 3, screenHeight * 33 / 100, screenWidth / 3, screenHeight / 20);
+
+		// --- Bottoni toggle 1 / 2 giocatori ---
+		int btnW = screenWidth / 7;
+		int btnH = screenHeight / 14;
+		int btnY = screenHeight * 39 / 100;
+		int centerX = screenWidth / 2;
+
+		this.btn1Player = new JToggleButton("1 GIOCATORE");
+		styleToggle(btn1Player, true);
+		btn1Player.setBounds(centerX - btnW - 8, btnY, btnW, btnH);
+
+		this.btn2Players = new JToggleButton("2 GIOCATORI");
+		styleToggle(btn2Players, false);
+		btn2Players.setBounds(centerX + 8, btnY, btnW, btnH);
+
+		this.playerGroup = new ButtonGroup();
+		playerGroup.add(btn1Player);
+		playerGroup.add(btn2Players);
+		btn1Player.setSelected(true);
+
+		// Aggiorna visibilità campi nome al cambio selezione
+		btn1Player.addActionListener(e -> updatePlayerFields(false));
+		btn2Players.addActionListener(e -> updatePlayerFields(true));
+
+		// --- Campo nome Giocatore 1 ---
+		// Con 1 giocatore usa le freccette; con 2 giocatori usa WASD (sta nella metà sinistra dello schermo)
+		this.nameLabel1 = new JLabel("NOME GIOCATORE 1 (Freccette):");
+		styleLabelField(nameLabel1);
+		nameLabel1.setBounds(screenWidth / 3, screenHeight * 51 / 100, screenWidth / 3, screenHeight / 20);
+
+		this.nameField1 = new JTextField();
+		styleTextField(nameField1);
+		nameField1.setBounds(screenWidth / 3, screenHeight * 57 / 100, screenWidth / 3, screenHeight / 20);
+
+		// --- Campo nome Giocatore 2 (metà destra dello schermo, usa le freccette) ---
+		this.nameLabel2 = new JLabel("NOME GIOCATORE 2 (Freccette):");
+		styleLabelField(nameLabel2);
+		nameLabel2.setBounds(screenWidth / 3, screenHeight * 64 / 100, screenWidth / 3, screenHeight / 20);
+		nameLabel2.setVisible(false);
+
+		this.nameField2 = new JTextField();
+		styleTextField(nameField2);
+		nameField2.setBounds(screenWidth / 3, screenHeight * 70 / 100, screenWidth / 3, screenHeight / 20);
+		nameField2.setVisible(false);
+	}
+
+	private void styleToggle(JToggleButton btn1Player2, boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private Object updatePlayerFields(boolean b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void styleTextField(JTextField nameField22) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void styleLabelField(JLabel nameLabel12) {
+		// TODO Auto-generated method stub
+		
+	}
 }
