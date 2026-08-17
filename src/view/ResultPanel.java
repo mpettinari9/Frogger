9774e0ef4961b2d4533a4faecadc9ea595384650
mpettinari9 <matildepettinari2004@gmail.com>
2026-08-17 09:41:42 +0200

    Add setter and getter in ResultPanel

--- src/view/ResultPanel.java
+++ src/view/ResultPanel.java
@@ -23,9 +23,6 @@ public class ResultPanel extends JPanel {
 		this.setSize(screenWidth, screenHeight);
 		this.setLayout(null);
 
-	
-
-
 		// --- Riga risultato Giocatore 1 ---
 		this.frogResultTextP1 = new JLabel("");
 		this.frogResultTextP1.setForeground(VERDE_RANA);
@@ -44,5 +41,52 @@ public class ResultPanel extends JPanel {
 		// Sotto la riga del giocatore 1, ancora sopra il bottone
 		this.frogResultTextP2.setBounds(screenWidth / 10, screenHeight * 57 / 100, screenWidth * 8 / 10, screenHeight / 12);
 		this.frogResultTextP2.setVisible(false);
+	    
+		
+		// --- Bottone Ricomincia: in fondo, non si sovrappone alle label ---
+		this.toTitlePanelButton = new JButton("RICOMINCIA");
+		this.toTitlePanelButton.setBackground(BLU_BOTTONE);
+		this.toTitlePanelButton.setForeground(BIANCO);
+		this.toTitlePanelButton.setFont(new Font("DIALOG", Font.BOLD, 24));
+		this.toTitlePanelButton.setFocusPainted(false);
+		this.toTitlePanelButton.setBorderPainted(false);
+		this.toTitlePanelButton.setBounds(screenWidth / 3, screenHeight * 72 / 100, screenWidth / 3, screenHeight / 10);
+
+		this.add(this.frogResultTextP1);
+		this.add(this.frogResultTextP2);
+		this.add(this.toTitlePanelButton);
 	}
+
+			
+
+	// Imposta il risultato del Giocatore 1
+	public void setFrogResultTextP1(String text) {
+		this.frogResultTextP1.setText(text);
+	}
+
+	// Imposta il risultato del Giocatore 2; nasconde la riga se vuota (partita a 1 giocatore)
+	public void setFrogResultTextP2(String text) {
+		this.frogResultTextP2.setText(text);
+		this.frogResultTextP2.setVisible(!text.isEmpty());
+	}
+
+	public JButton getToTitlePanelButton() {
+		return toTitlePanelButton;
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		// Sfondo immagine
+		g.drawImage(new ImageIcon("src/view/Asset/sfondofrogger.png").getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+		// Rettangolo semitrasparente dietro le info per migliorare la leggibilità
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setColor(PANNELLO_BG);
+		g2.fillRoundRect(screenWidth() / 10, screenHeight() * 42 / 100,
+			screenWidth() * 8 / 10, screenHeight() * 36 / 100, 20, 20);
+	}
+
+	// Helper per leggere dimensioni attuali del pannello
+	private int screenWidth()  { return getWidth();  }
+	private int screenHeight() { return getHeight(); }
 }
